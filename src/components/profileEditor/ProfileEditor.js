137e4a971b2ed0968cac12527589f39ccce89417
Melina Chamayou <Melina@Melinas-MacBook-Pro.local>
2020-07-23 12:27:41 +0200

    clean variables name and start adding typescript

--- src/components/profileEditor/ProfileEditor.js
+++ src/components/profileEditor/ProfileEditor.js
@@ -1,63 +1,20 @@
 import React from 'react';
-import ResourceProfileMapping from '../resourceProfileMapping/ResourceProfileMapping';
-import { useQuery } from '@apollo/react-hooks';
-import { loader } from 'graphql.macro';
+import { useParams } from "react-router-dom";
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
+const ResourceProfileMapping = (props) => {
+    let { resourceId } = useParams()
 
-
-const getResources = loader(
-    'src/graphql/queryProfile.gql'
-);
-
-const Menu = () => {
-    const source = "Patient";
-    const { data: resourceData, loading: resourceLoading, error: resourceError } = useQuery(getResources, {variables: { definitionId: source }});
-    const { data: resourceStructureDef, loading: structureDefLoading, error: structureDefError } = useQuery(getResources, {variables: { definitionId: "StructureDefinition" }})
-
-    if (resourceLoading) return <span>Loading</span>
-    if (resourceError) return <span>Error</span>
-    if (structureDefLoading) return <span>Loading</span>
-    if (structureDefError) return <span>Error</span>
-
-    const structureDefinition = resourceData.structureDefinition;
-    const attributes = resourceData.structureDefinition.attributes;
-
-    const renderTopic = attributes.map((attribute, i) => {
-        const attributeName = attribute.attribute.id;
+    if (resourceId === 'profileSettings'){
         return (
-            <li key={i}>
-                <Link to={`/${structureDefinition.id}/${attributeName}`}>{attributeName}</Link>
-            </li>
-        )
-    });
-
-    console.log(resourceData, resourceStructureDef);
-
-    return (
-        <Router>
-        <div>
-            <ul>
-                <li>
-                    <Link to={`/${structureDefinition.id}/profileSettings`}>
-                        profile settings
-                    </Link>
-                </li>
-                {renderTopic}
-            </ul>
-            <Switch>
-            <Route path="/:profile/:topicId">
-                <ResourceProfileMapping everything={resourceData} structure={resourceStructureDef}/>
-            </Route>
-            </Switch>
-        </div>
-        </Router>
-    )
+            <div>Profile Settings</div>
+        );
+    } else {
+        return (
+            <div>
+                <h2>{ resourceId }</h2>
+            </div>
+        );
+    }
 }
 
-export default Menu;
+export default ResourceProfileMapping;
