2cb6ac275ddc34d7d23552ff11396fd0a6a1d05f
Melina <47388675+Klochette@users.noreply.github.com>
2021-03-15 17:23:18 +0100

    remove token

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,2 @@
 export const FHIR_API_URL = 'https://staging.arkhn.com/api';
-export const AUTH_TOKEN_KEY =
-  'XWHU7-g7gqIP0mA4X4i2iRlfpX8KGo59yA5nGLQIBZU.uxVbWMquClinW_Aootuy9y-01s0ATHaQfbQGuR6U_YU';
+export const AUTH_TOKEN_KEY = '';
