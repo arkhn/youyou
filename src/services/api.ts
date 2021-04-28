6a0149a4b81f953237e74714200a570ac868bc15
Melina <47388675+Klochette@users.noreply.github.com>
2021-04-28 17:01:15 +0200

    delete console log

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { AUTH_TOKEN_KEY, FHIR_API_URL } from '../constants';
 
-console.log(AUTH_TOKEN_KEY);
-
 const api = axios.create({
   baseURL: FHIR_API_URL,
   headers: {
